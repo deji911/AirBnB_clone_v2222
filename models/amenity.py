9b07480ec3f04c31b7a8ed38190d6777cb2c350c
Ayodeji Isaac Ariyo <isaacayodejiariyo@gmail.com>
2023-07-27 07:46:03 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
-
 
 class Amenity(BaseModel, Base):
     '''amenity class'''
     __tablename__ = 'amenities'
-    if storage_type == 'db':
+    if 'HBNB_TYPE_STORAGE' in os.environ and os.environ['HBNB_TYPE_STORAGE'] == 'db':
         name = Column(String(128), nullable=False)
     else:
         name = ""
