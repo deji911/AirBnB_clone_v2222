fea281d741acbaf42d3708486354b0dd3d3f5c7c
Ayodeji Isaac Ariyo <isaacayodejiariyo@gmail.com>
2023-07-27 08:04:08 +0100

    Update __init__.py

---
+++ models/engine/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Instantiates a storage object.
+
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
+import os  # Import the os module to access environmental variables
+
+if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
+
+# Now you can access storage_type as an attribute of the storage object
+storage_type = os.environ.get("HBNB_TYPE_STORAGE")
